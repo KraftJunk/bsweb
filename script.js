f23db32a1db8911212c715eff6186b9940009c0a
EEJ McGuinness <96074540+KraftJunk@users.noreply.github.com>
2023-08-02 14:58:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    var typed = new Typed(".typed", {
+        strings: ["Architect" , "Interior Designer", "3D Modelling."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 800,
+        showCursor: false
+    })
